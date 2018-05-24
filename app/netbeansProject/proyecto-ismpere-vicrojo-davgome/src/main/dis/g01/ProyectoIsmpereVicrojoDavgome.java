1196f5879314316a7e93a6c821f147a22ac27ec4
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-05-25 00:05:04 +0200

    Finalizo la creación del proyecto en netbeans

---
+++ app/netbeansProject/proyecto-ismpere-vicrojo-davgome/src/main/dis/g01/ProyectoIsmpereVicrojoDavgome.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.dis.g01;
+
+/**
+ *
+ * @author ismael
+ */
+public class ProyectoIsmpereVicrojoDavgome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
