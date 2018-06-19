1ac16fc350ff2b47e0b4fa98fe7b3d7d04169345
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-06-20 00:55:42 +0200

    Añado la libreria Json

--- app/netbeansProject/PracticaDIS/src/es/uva/eii/dis/floristeriaHolanda/Negocio/modelos/Empleado.java
+++ app/netbeansProject/PracticaDIS/src/es/uva/eii/dis/floristeriaHolanda/Negocio/modelos/Empleado.java
@@ -6,6 +6,9 @@
 package es.uva.eii.dis.floristeriaHolanda.Negocio.modelos;
 
 import es.uva.eii.dis.floristeriaHolanda.Persistencia.FachadaPersistencia;
+import java.util.Date;
+import java.util.ArrayList;
+import org.json;
 
 /**
  *
@@ -13,16 +16,36 @@ import es.uva.eii.dis.floristeriaHolanda.Persistencia.FachadaPersistencia;
  */
 public class Empleado {
     
-    public Empleado(){
+    private String nif;
+    private String nombre;
+    private Date fechaIni;
+    private ArrayList<Rol> listaRoles;
+    private ArrayList<VinculacionConLaEmpresa> listaVinculaciones;
+    private ArrayList<Disponibilidad> listaDisponibilidades;
+    
+    public Empleado(String res){
+        /* Pasar el string JsonArray a JsonArray */
+        JsonParser parser = new JsonParser();
+        JsonElement tradeElement = parser.parse(res);
+        JsonObject json = tradeElement.getAsJsonObject();
         
+        
+        
+        nif = json.get("NIF").toString();
+    }
+    
+    public String getNif(){
+        return nif;
     }
     
     public static Empleado getEmpleadoPorDNIyPass(String d, String p){
-        String h = FachadaPersistencia.getEmpleadoPorDNIyPass(d, p);
+        String res = FachadaPersistencia.getEmpleadoPorDNIyPass(d, p);
         Empleado e = null;
-        if(h!=null){
-            e = new Empleado();
-            System.out.println(h);
+        if(res!=null){
+            e = new Empleado(res);
+            String prueba = "aaaaa";
+            System.out.println(e.getNif());
+            System.out.println(prueba);
         }
         return e;
     }
