ab6510074502555eac757e51285fdc91e95777ae
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-06-20 16:45:29 +0200

    Update para David

--- app/netbeansProject/PracticaDIS/src/es/uva/eii/dis/floristeriaHolanda/Persistencia/EmpleadoDB.java
+++ app/netbeansProject/PracticaDIS/src/es/uva/eii/dis/floristeriaHolanda/Persistencia/EmpleadoDB.java
@@ -18,7 +18,7 @@ public class EmpleadoDB {
         
         String e = null;
         
-        String SQL_Query_Empleado = "SELECT * FROM Empleado E WHERE E.Nif = '" + d + "' and E.Password = '" + p + "'";    
+        String SQL_Query_Empleado = "SELECT * FROM Empleado E WHERE E.Nif = '" + d + "' and E.Password = '" + p + "'";
         String SQL_Query_Roles = "SELECT * FROM RolesEnEmpresa R WHERE R.Empleado = '" + d + "' ORDER BY R.ComienzoEnRol";
         String SQL_Query_Vinculaciones = "SELECT * FROM VinculacionConLaEmpresa V WHERE V.Empleado = '" + d + "' ORDER BY V.inicio";
         String SQL_Query_Disponibilidades = "SELECT * FROM DisponibilidadEmpleado D WHERE D.Empleado = '" + d + "' ORDER BY D.Comienzo";
@@ -48,5 +48,29 @@ public class EmpleadoDB {
         }
         
         return e;
+    }
+    
+    public static String getRolesEmpleadoPorDNI(String d){
+        String SQL_Query_Roles = "SELECT * FROM RolesEnEmpresa R WHERE R.Empleado = '" + d + "' ORDER BY R.ComienzoEnRol";
+        
+        ConexionDB conexion = ConexionDB.getInstancia(); 
+        
+        return conexion.consulta(SQL_Query_Roles);
+    }
+    
+    public static String getVinculacionesEmpleadoPorDNI(String d){
+        String SQL_Query_Vinculaciones = "SELECT * FROM VinculacionConLaEmpresa V WHERE V.Empleado = '" + d + "' ORDER BY V.inicio";
+        
+        ConexionDB conexion = ConexionDB.getInstancia(); 
+        
+        return conexion.consulta(SQL_Query_Vinculaciones);
+    }
+    
+    public static String getDisponibilidadesEmpleadoPorDNI(String d){     
+        String SQL_Query_Disponibilidades = "SELECT * FROM DisponibilidadEmpleado D WHERE D.Empleado = '" + d + "' ORDER BY D.Comienzo";
+        
+        ConexionDB conexion = ConexionDB.getInstancia(); 
+        
+        return conexion.consulta(SQL_Query_Disponibilidades);
     }
 }
